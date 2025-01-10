43c8733d0da24e26e3f4290f3c3af9c5acc52692
O.J. Paul <jpaul9327@gmail.com>
2025-01-10 08:17:56 +0300

    Make more animations

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Send email using EmailJS
 const contactForm = document.forms[0];
 const serviceID = "service_mf8kagg";
 const contactFormID = contactForm.getAttribute("id");
@@ -13,3 +14,33 @@ function sendEmail(e) {
             console.log('FAILED...', error);
         });
 }
+
+// Show selected nav link with .active class
+const navLinks = document.querySelectorAll("nav a");
+
+navLinks.forEach(link => {
+    link.onclick = (() => {
+        setTimeout(() => {
+            navLinks.forEach(l => l.classList.remove("active"));
+            link.classList.add("active");
+        }, 300);
+    });
+});
+
+const about = document.querySelector("#about");
+const skills = document.querySelector("#skills");
+const work = document.querySelector("#work");
+const contact = document.querySelector("#contact");
+
+const sections = [about, skills, work, contact];
+
+window.onscroll = (() => {
+    sections.forEach((section, i) => {
+        let sectionPositionY = section.getBoundingClientRect().y;
+        const topOffset = 235;
+        if (sectionPositionY < (window.innerHeight - topOffset)) {            
+            navLinks.forEach(l => l.classList.remove("active"));
+            navLinks[i].classList.add("active");
+        }
+    });
+});
